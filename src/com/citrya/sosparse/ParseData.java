45100bed64c3745e30c66e764baa94d5812cbd97
chetankumar81 <chetantulsyan15@gmail.com>
2017-11-30 06:51:45 +0530

    implemented regex grouping backreference

--- src/com/citrya/sosparse/ParseData.java
+++ src/com/citrya/sosparse/ParseData.java
@@ -1,43 +1,20 @@
 package com.citrya.sosparse;
 
-import java.sql.Connection;
-import java.util.Iterator;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.JSONValue;
-
 import com.citrya.dao.DatabaseAccess;
 
 public class ParseData {
-	private Connection currentCon;
 	private String shopname;
-	
+
 	public ParseData(String shopname)
 	{
 		this.shopname = shopname;
 	}
-	
+
 	public String[] getResult(String text)
 	{
 		String[] result = new String[3]; //for storing all 3 values
-		/*Object obj1 =JSONValue.parse(response);  
-		JSONObject jsonObject = (JSONObject) obj1;
-		JSONArray responsearray = (JSONArray) jsonObject.get("responses");
-		@SuppressWarnings("rawtypes")
-		Iterator i =responsearray.iterator();
-		String text= null;
-		while (i.hasNext()) {
-            JSONObject obj2 = (JSONObject) i.next();
-             JSONObject obj3=(JSONObject)obj2.get("fullTextAnnotation");
-             text=(String)obj3.get("text");
-        }
-		//result[0]= text;*/
-		
+
 		ParseMatchData parsedata = new DatabaseAccess().getmatchdetails(shopname);
-		String returnvalue="";
 		String[] lines = text.split("\n");
 		if(parsedata.normalparse == 1){
 			for (int i1=0;i1<lines.length;i1++)
@@ -55,80 +32,62 @@ public class ParseData {
 						result[0] = temp[temp.length-1];
 					}
 				}
-						
+
 				//date
 				String[] tempdateseq = parsedata.dateparse.split("&");
 				if ((lines[i1]).contains(tempdateseq[0])) {
 					System.out.println(" date lines[i1]"+lines[i1]);
-				if (tempdateseq[1].equalsIgnoreCase("1"))
-						{
-					String[] temp = lines[i1].split(" ");
-					if (temp.length==4)
-					
-					result[1] = temp[temp.length-3]+"-"+temp[temp.length-2];
-					else
-						result[1] = temp[temp.length-2];
-						}
-				else if(tempdateseq[1].equalsIgnoreCase("3")){
-					result[1] = lines[i1];
+					if (tempdateseq[1].equalsIgnoreCase("1"))
+					{
+						String[] temp = lines[i1].split(" ");
+						if (temp.length==4)
+
+							result[1] = temp[temp.length-3]+"-"+temp[temp.length-2];
+						else
+							result[1] = temp[temp.length-2];
+					}
+					else if(tempdateseq[1].equalsIgnoreCase("3")){
+						result[1] = lines[i1];
+					}
 				}
-				}
-				
+
 				// amount
-				
+
 				String[] tempamountseq = parsedata.amounparse.split("&");
-				
-				if ((lines[i1]).contains(tempamountseq[0])) {
+				if ((lines[i1]).contains(tempamountseq[0])){
 					System.out.println("cash lines[i1]"+lines[i1]);
-				
+
 					result[2] =lines[i1+new Integer(tempamountseq[2]).intValue()];
-					
 				}
-			}
+			}	
 		}else if(parsedata.normalparse == 2){
 			for (int i1=0;i1<lines.length;i1++)
 			{
+				//This is backrefernce grouping in regular expression;
+				//What it did is first pick the line which matches this regular expression and then store the result what you want
+				//in that line as group 1 and get that group 1 using $1 as a second parameter in replaceAll method
+
 				//invoice number.
 				if(lines[i1].matches(parsedata.billnoparse)){
-					result[0] = lines[i1];
+
+					result[0] = lines[i1].replaceAll(parsedata.billnoparse,"$1"); 
 				}
-				
-				/*if(lines[i1].matches(parsedata.billnoparse)){
-					Matcher m = Pattern.compile(parsedata.billnoparse).matcher(lines[i1]);
-					if(m.find()){
-						result[0] = lines[i1].substring(m.start(),m.end());
-					}
-				}*/
-					 
-					
-				/*String[] tempinvseq = parsedata.billnoparse.split("&");
-				if((lines[i1]).contains(tempinvseq[0])){
-					System.out.println("cash lines["+i1+"]: "+lines[i1]);
-					result[0] = lines[i1+new Integer(tempinvseq[2]).intValue()];
-				}*/
-				
+
 				//date
-				String[] tempdateseq = parsedata.dateparse.split("&");
-				if((lines[i1]).contains(tempdateseq[0])){
-					System.out.println("cash lines["+i1+"]: "+lines[i1]);
-					if (tempdateseq[3].equalsIgnoreCase("3"))
-					{
-						result[1] = lines[i1+new Integer(tempdateseq[2]).intValue()];
-					}
+				if(lines[i1].matches(parsedata.dateparse)){
+					result[1] = lines[i1].replaceAll(parsedata.dateparse,"$1"); 
 				}
-				
+
 				// amount
 				String[] tempamountseq = parsedata.amounparse.split("&");
 				if ((lines[i1]).equals(tempamountseq[0])) {
 					System.out.println("cash lines[i1]"+lines[i1]);
-				
+
 					result[2] =lines[i1+new Integer(tempamountseq[2]).intValue()];
-					
+
 				}
 			}
 		}
-		
 		return result;
-		
 	}
 }
